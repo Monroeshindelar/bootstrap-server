cc9dd2333a70233a4bd6a9fd23ee207fafd481ef
Monroe Shindelar <monroeshindelar@gmail.com>
2024-06-15 08:30:30 -0700

    Various fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,17 @@ IFS=" "
 BOOTSTRAP_KUBE_NODE=false
 BOOTSTRAP_REBOOT=false
 BOOTSTRAP_SKIP_HOSTS=false
+HELPERS_PATH="helpers"
 SSH_CONFIG_PATH="ssh"
 ZSH_CONFIG_PATH="zsh"
 
 print_usage() {
     printf "Usage: ..."
 }
+
+sudo apt-get update
+
+chmod +x $HELPERS_PATH/hostsHelper.sh
 
 while getopts 'ghks' flag; do
     case "${flag}" in
@@ -34,24 +39,12 @@ done
 
 if [[ ! -z "${BOOTSTRAP_HOSTNAME}" ]]; then
     echo "Setting hostname to ${BOOTSTRAP_HOSTNAME}"
-    hostnamectl set-hostname ${BOOTSTRAP_HOSTNAME}
+    sudo hostnamectl set-hostname ${BOOTSTRAP_HOSTNAME}
 fi
 
 # Adding entries to the host file
 if [ ! "${BOOTSTRAP_SKIP_HOSTS}" ]; then
-    echo "Configuring hosts file"
-    echo "# Added by bootstrap script" >> /etc/hosts
-    echo "127.0.1.1  $(hostname).local   $(hostname)" >> /etc/hosts
-
-    while read -ra line; do
-        name=${line[0]}
-        ip=${line[1]}
-
-        if [ "${hostname}" != "$name" ]; then
-        echo "Configuring host for ${name}"
-            echo "${ip}  ${name}.local  ${name}" >> /etc/hosts
-        fi
-    done < hosts/hosts
+    sudo $HELPERS_PATH/hostsHelper.sh
 fi
 
 # Set up SSH server with auth keys from github
@@ -60,33 +53,30 @@ if [[ ! -z "${BOOTSTRAP_GH_USER}" ]]; then
     echo "Pulling auth keys from github user ${BOOTSTRAP_GH_USER}"
     ssh-import-id-gh $BOOTSTRAP_GH_USER
 fi
-cp $SSH_CONFIG_PATH/sshd_config /etc/ssh/sshd_config
-systemctl enable sshd && systemctl restart sshd
+sudo cp $SSH_CONFIG_PATH/sshd_config /etc/ssh/sshd_config
+sudo systemctl enable sshd && sudo systemctl restart sshd
 
 # Install some packages
-apt-get update
-apt-get install -y ca-certificates curl apt-transport-https ca-certificates curl gpg nfs-common
+sudo apt-get install -y ca-certificates curl apt-transport-https ca-certificates curl gpg nfs-common
 
 # Setup repository for docker
-echo "Setting up docker repository"
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Setup repository for kube
 echo "Setting up kube repository"
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
-
-apt-get update
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Install and configure zsh and plugins
 echo "Installing and configuring zsh..." 
-apt-get install zsh -y
+sudo apt-get install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
@@ -95,23 +85,23 @@ cp $ZSH_CONFIG_PATH/themes/* $ZSH/themes
 
 # Install docker
 echo "Installing docker..."
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Necessary configuration for containerd to work
 echo "Configuring containerd for kubernetes"
-mkdir -p /etc/containerd/
-containerd config default | tee /etc/containerd/config.toml
-sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo mkdir -p /etc/containerd/
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 
 # Install kubernetes  
 if [ "$BOOTSTRAP_KUBE_NODE" ]; then
     echo "Installing kubernetes"  
-    apt-get install -y kubelet kubeadm kubectl kubectx
-    apt-mark hold kubelet kubeadm kubectl
-    systemctl enable --now kubelet
+    sudo apt-get install -y kubelet kubeadm kubectl kubectx
+    sudo apt-mark hold kubelet kubeadm kubectl
+    sudo systemctl enable --now kubelet
 fi
 
-apt-get update && apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 
 # Set the default shell to zsh
 sudo chsh -s "$(which zsh)" $USER
---
+++ helpers/hostsHelper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Configuring hosts file"
+echo "# Added by bootstrap script" >> /etc/hosts
+echo "127.0.1.1  $(hostname).local   $(hostname)" >> /etc/hosts
+
+while read -ra line; do
+    name=${line[0]}
+    ip=${line[1]}
+
+    if [ "${hostname}" != "$name" ]; then
+        echo "Configuring host for ${name}"
+        echo "${ip}  ${name}.local  ${name}" >> /etc/hosts
+    fi
+done < hosts/hosts
